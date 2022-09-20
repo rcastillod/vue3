625bb9f31ac5b54b51215a871b50819af5b15d9e
rcastillod <rodrigo.castillo@global3.cl>
2022-09-20 17:55:13 -0300

    feat: Single file components

---
+++ src/components/EventCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="event-card">
+    <span>@{{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: {
+      type: Object,
+    },
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid hsl(213, 23%, 70%);
+  border-radius: 5px;
+  transition: all 0.3s ease-in-out;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+
+.event-card > span {
+  color: hsl(213, 23%, 50%);
+}
+</style>
